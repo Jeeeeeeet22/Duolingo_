b3f9f8475e1b1240a9d009f5060cbc110759b138
Jeet Sahoo <jeetsahoo2203@gmail.com>
2025-07-15 17:31:07 +0530

    Add files via upload

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -10,7 +10,11 @@ import { ReceiptRussianRuble } from "lucide-react";
 import { upsertChallengeProgress } from "@/actions/challenge-progress";
 import {toast} from "sonner";
 import { reduceHearts } from "@/actions/user-progress";
-import { useAudio } from "react-use";
+import { useAudio,useWindowSize } from "react-use";
+import Image from "next/image";
+import { ResultCard } from "./result-card";
+import { useRouter } from "next/navigation";
+import Confetti from "react-confetti"
 
 type Props = {
     initialPercentage: number;
@@ -32,8 +36,11 @@ export const Quiz = ({
     initialLessonChallenges,
     userSubscription,
 }: Props) => {
+    const {width,height}=useWindowSize();
+    const router=useRouter();
     const[correctAudio,_c,correctControls]=useAudio({src:"/correct.wav"});
     const[incorrectAudio,_i,incorrectControls]=useAudio({src:"/incorrect.wav"});
+    const[lessonId]=useState(initialLessonId)
     const [Pending, startTransition] = useTransition();
     const [hearts, setHearts] = useState(initialHearts || 50);
     const [percentage, setPercentage] = useState(() => initialPercentage);
@@ -47,7 +54,7 @@ export const Quiz = ({
     const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
 
     const challenge = challenges[activeIndex];
-    const options = challenge.challengeOptions;
+    const options = challenge?.challengeOptions??[];
 
     const onNext = () => {
         setActiveIndex((current) => current + 1);
@@ -124,6 +131,57 @@ export const Quiz = ({
         }
     }
 
+
+    if(true||!challenge){
+        return(
+            <>
+            <Confetti
+            width={width}
+            height={height} 
+            recycle={false}
+            numberOfPieces={500}
+            tweenDuration={10000}
+            />
+            <div className="flex flex-col gapy-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+                <Image 
+                 src="/finish.svg"
+                 alt="Finish"
+                 className="hidden lg:block"
+                 height={100} 
+                 width={100}
+                />
+
+                <Image 
+                 src="/finish.svg"
+                 alt="Finish"
+                 className="block lg:hidden"
+                 height={100} 
+                 width={50}
+                />
+                <h1 className="text-xl lg:test-3xl font-bold text-neutral-700">
+                    Great Job!<br/>You've completed the lesson!
+                </h1>
+                <div className="flex items-center gap-x-4 w-full">
+                    <ResultCard 
+                    variant="points"
+                    value={challenges.length*10}
+                    />
+
+                    <ResultCard 
+                    variant="hearts"
+                    value={hearts}
+                    />
+                </div>
+            </div>
+            <Footer  
+             lessonId={lessonId}
+             status="completed"
+             onCheck={()=>{}}
+             />
+            </>
+        )
+    }
+
     const title =
         challenge?.type === "ASSIST"
             ? "Select the correct meaning"
@@ -131,8 +189,11 @@ export const Quiz = ({
 
     return (
         <>
-        {incorrectAudio}
-        {correctAudio}
+       <div style={{ display: "none" }}>
+  {correctAudio}
+  {incorrectAudio}
+</div>
+
             <Header
                 hearts={hearts}
                 percentage={percentage}
